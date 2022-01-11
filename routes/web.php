6a3334c227914771a3bd70427119c984a92a1507
Ezzat Makar <ezzatmakar93@gmail.com>
2022-01-11 23:02:39 +0200

    update the web route file

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('dashboard');
-});
+// Route::get('/', function () {
+//     return view('dashboard');
+// });
+
+// another way to get view
+Route::view('', 'dashboard');
